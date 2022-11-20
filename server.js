b0a2af50097e71ec0536e32284829a38ee3e0fc7
Ssadiq Roy <ssadiqueroy@gmail.com>
2022-11-20 00:41:32 +0000

    hs

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const dotenv = require('dotenv');
+const { WriteError } = require('./error/writeError');
 
 process.on('uncaughtException', (error) => {
   // console.log(error);
-  new WriteError(error, undefined, 'uncaughtException');
+  new WriteError(error, {}, 'uncaughtException');
   process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
 
+new WriteError({ m: 'Before we create the app' }, {}, 'just console');
 const app = require('./app');
-const { WriteError } = require('./error/writeError');
 
-new WriteError({ m: 'We are about to start the server' }, undefined, 'just console');
+new WriteError({ m: 'We are about to start the server' }, {}, 'just console');
 const server = app.listen(process.env.port, process.env.host, () => console.log('🎁🎁 blacfront server started...'));
 
 process.on('unhandledRejection', (error) => {
   // console.log(error);
-  new WriteError(error, undefined, 'unhandledRejection');
+  new WriteError(error, {}, 'unhandledRejection');
   server.close(() => {
     process.exit();
   });
