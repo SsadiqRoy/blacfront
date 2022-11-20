36b4f712a0a83dea8adb3c2a0ea170423c44b369
Ssadiq Roy <ssadiqueroy@gmail.com>
2022-11-20 00:29:07 +0000

    changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieparser());
-app.use(cors({ origin: [], credentials: true }));
+// app.use(cors({ origin: [], credentials: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
