13eaeff90074b5906aae2f6f53c79da600e292c2
Ssadiq Roy <ssadiqueroy@gmail.com>
2022-12-10 22:58:54 +0000

    repair-home-feed

--- public/javascript/controller/client/home.js
+++ public/javascript/controller/client/home.js
@@ -4,7 +4,7 @@ import * as model from '../../model/model.js';
 async function controlHeadingSlide() {
   try {
     const response = await model.get(
-      '/movies?fields=title,landscape,description,id&limit=10&rating=gte,5.5&order=createdAt,dsc'
+      '/movies?fields=title,landscape,description,id&limit=10&rating=gte,5.5&order=createdAt,desc'
     );
     view.renderHeadingSlide(response);
   } catch (error) {
@@ -15,10 +15,10 @@ async function controlHeadingSlide() {
 
 async function fillMovies() {
   try {
-    const response = await model.getfull('/movies?fields=title,portrait,rating,id&limit=20&order=releasedDate,dsc');
+    const response = await model.getfull('/movies?fields=title,portrait,rating,id&limit=20&order=releasedDate,desc');
     view.renderFillSliders({ response, containerId: 'first-movies', type: 'movie', cardName: 'movieCard' });
 
-    const res2 = await model.getfull('/movies?fields=title,portrait,rating,id&limit=20&page=2&order=releasedDate,dsc');
+    const res2 = await model.getfull('/movies?fields=title,portrait,rating,id&limit=20&page=2&order=releasedDate,desc');
     view.renderFillSliders({ response: res2, containerId: 'second-movies', type: 'movie', cardName: 'movieCard' });
   } catch (error) {
     console.log(error);
@@ -28,10 +28,10 @@ async function fillMovies() {
 
 async function fillSeries() {
   try {
-    const response = await model.getfull('/series?fields=title,portrait,rating,id&limit=20&order=releasedDate,dsc');
+    const response = await model.getfull('/series?fields=title,portrait,rating,id&limit=20&order=releasedDate,desc');
     view.renderFillSliders({ response, containerId: 'first-series', type: 'serie', cardName: 'movieCard' });
 
-    const res2 = await model.getfull('/series?fields=title,portrait,rating,id&limit=20&page=2&order=releasedDate,dsc');
+    const res2 = await model.getfull('/series?fields=title,portrait,rating,id&limit=20&page=2&order=releasedDate,desc');
     view.renderFillSliders({ response: res2, containerId: 'second-series', type: 'serie', cardName: 'movieCard' });
   } catch (error) {
     await model.localPost('/write-to-log', error);
@@ -41,10 +41,10 @@ async function fillSeries() {
 
 async function fillGames() {
   try {
-    const response = await model.getfull('/games?fields=title,landscape,rating,id&limit=20&order=releasedDate,dsc');
+    const response = await model.getfull('/games?fields=title,landscape,rating,id&limit=20&order=releasedDate,desc');
     view.renderFillSliders({ response, containerId: 'first-games', type: 'game', cardName: 'gameCard' });
 
-    const res2 = await model.getfull('/games?fields=title,landscape,rating,id&limit=20&page=2&order=releasedDate,dsc');
+    const res2 = await model.getfull('/games?fields=title,landscape,rating,id&limit=20&page=2&order=releasedDate,desc');
     view.renderFillSliders({ response: res2, containerId: 'second-games', type: 'game', cardName: 'gameCard' });
   } catch (error) {
     await model.localPost('/write-to-log', error);
