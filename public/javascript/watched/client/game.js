152338653a4cb46d6ade693b274e2e3612bb086a
Ssadiq Roy <ssadiqueroy@gmail.com>
2022-12-10 23:54:29 +0000

    wi

--- public/javascript/watched/client/game.js
+++ public/javascript/watched/client/game.js
@@ -1110,8 +1110,8 @@ parcelHelpers.export(exports, "main_url", ()=>main_url);
 parcelHelpers.export(exports, "countries", ()=>countries);
 parcelHelpers.export(exports, "serieStatus", ()=>serieStatus);
 parcelHelpers.export(exports, "resolutions", ()=>resolutions);
-const api_url = "https://api.blaciris.com/v1";
-const main_url = "https://blaciris.com";
+const api_url = "https://staging.api.blaciris.com/v1";
+const main_url = "https://staging.blaciris.com";
 const countries = [
     "Afghanistan",
     "Albania",
