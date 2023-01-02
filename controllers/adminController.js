0d4224413e494e3bcca813ec8df299fe42884efa
Ssadiq Roy <ssadiqueroy@gmail.com>
2023-01-02 10:14:39 +0000

    sort

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,123 +1,138 @@
-const { catchAsync, getRequest, stringifyQuery } = require('../utils/utils');
+const { catchAsync, getRequest, stringifyQuery } = require("../utils/utils");
 const jsPath = `/javascript/${process.env.js_folder}/admin`;
 // const jsPath = `/javascript/watched/admin`;
 
 //
 exports.profile = catchAsync(async (req, res, next) => {
   const ext = {
-    title: 'Account Profile',
-    page: 'profile',
-    searchPlaceholder: '',
+    title: "Account Profile",
+    page: "profile",
+    searchPlaceholder: "",
     js: `${jsPath}/profile`,
   };
 
-  res.status(200).render('admin/profile', { ext });
+  res.status(200).render("admin/profile", { ext });
 });
 
 //
 exports.movies = catchAsync(async (req, res, next) => {
-  let query = '';
+  let query = "";
   if (req.query) query = stringifyQuery(req.query);
 
   // const { meta, data: movies } = await getRequest(req, `/movies${query}`);
-  const { meta, data: movies } = await getRequest(req, `/movies?limit=30&fields=id,landscape,title,rating,description`);
+  const { meta, data: movies } = await getRequest(
+    req,
+    `/movies?limit=30&fields=id,landscape,title,rating,description&order=createdAt,desc`
+  );
   // console.log(meta);
 
   const ext = {
-    title: 'Movies',
-    page: 'movies',
-    searchPlaceholder: 'in movies',
+    title: "Movies",
+    page: "movies",
+    searchPlaceholder: "in movies",
     js: `${jsPath}/movies`,
   };
 
   // console.log(movies);
-  res.status(200).render('admin/movies', { ext, meta, movies });
+  res.status(200).render("admin/movies", { ext, meta, movies });
 });
 
 //
 exports.series = catchAsync(async (req, res, next) => {
-  const { meta, data: series } = await getRequest(req, '/series?limit=30&fields=id,landscape,title,rating,description');
+  const { meta, data: series } = await getRequest(
+    req,
+    "/series?limit=30&fields=id,landscape,title,rating,description&order=createdAt,desc"
+  );
 
   const ext = {
-    title: 'series',
-    page: 'series',
-    searchPlaceholder: 'in series',
+    title: "series",
+    page: "series",
+    searchPlaceholder: "in series",
     js: `${jsPath}/series`,
   };
 
-  res.status(200).render('admin/series', { ext, meta, series });
+  res.status(200).render("admin/series", { ext, meta, series });
 });
 
 //
 exports.games = catchAsync(async (req, res, next) => {
   // console.log('about to hit');
-  const { meta, data: games } = await getRequest(req, '/games?limit=30&fields=id,landscape,title,rating,description');
+  const { meta, data: games } = await getRequest(
+    req,
+    "/games?limit=30&fields=id,landscape,title,rating,description&order=createdAt,desc"
+  );
   // console.log(games);
   const ext = {
-    title: 'games',
-    page: 'games',
-    searchPlaceholder: 'in games',
+    title: "games",
+    page: "games",
+    searchPlaceholder: "in games",
     js: `${jsPath}/games`,
   };
 
-  res.status(200).render('admin/games', { ext, meta, games });
+  res.status(200).render("admin/games", { ext, meta, games });
 });
 
 //
 exports.notifications = catchAsync(async (req, res, next) => {
-  const { meta, data: notifications } = await getRequest(req, '/notifications');
+  const { meta, data: notifications } = await getRequest(req, "/notifications");
 
   const ext = {
-    title: 'notifications',
-    page: 'notifications',
-    searchPlaceholder: 'in notifications',
+    title: "notifications",
+    page: "notifications",
+    searchPlaceholder: "in notifications",
     js: `${jsPath}/notifications`,
   };
 
-  res.status(200).render('admin/notifications', { ext, notifications, meta });
+  res.status(200).render("admin/notifications", { ext, notifications, meta });
 });
 
 //
 exports.schedules = catchAsync(async (req, res, next) => {
-  const { meta, data: schedules } = await getRequest(req, '/schedules?order=date&limit=2');
+  const { meta, data: schedules } = await getRequest(
+    req,
+    "/schedules?order=date&limit=2"
+  );
 
   const ext = {
-    title: 'schedules',
-    page: 'schedules',
-    searchPlaceholder: 'in schedules',
+    title: "schedules",
+    page: "schedules",
+    searchPlaceholder: "in schedules",
     js: `${jsPath}/schedules`,
   };
 
-  res.status(200).render('admin/schedules', { ext, schedules, meta });
+  res.status(200).render("admin/schedules", { ext, schedules, meta });
 });
 
 //
 exports.problems = catchAsync(async (req, res, next) => {
-  const { meta, data: problems } = await getRequest(req, '/problems');
+  const { meta, data: problems } = await getRequest(req, "/problems");
 
   const ext = {
-    title: 'problems',
-    page: 'problems',
-    searchPlaceholder: 'in problems',
+    title: "problems",
+    page: "problems",
+    searchPlaceholder: "in problems",
     js: `${jsPath}/problems`,
   };
 
-  res.status(200).render('admin/problems', { ext, problems, meta });
+  res.status(200).render("admin/problems", { ext, problems, meta });
 });
 
 //
 exports.suggestions = catchAsync(async (req, res, next) => {
-  const { meta, data: suggestions } = await getRequest(req, '/suggestions?limit=30&order=createdAt,asc');
+  const { meta, data: suggestions } = await getRequest(
+    req,
+    "/suggestions?limit=30&order=createdAt,asc"
+  );
   // console.log(suggestions);
 
   const ext = {
-    title: 'suggestions',
-    page: 'suggestions',
-    searchPlaceholder: 'in suggestions',
+    title: "suggestions",
+    page: "suggestions",
+    searchPlaceholder: "in suggestions",
     js: `${jsPath}/suggestions`,
   };
 
-  res.status(200).render('admin/suggestions', { ext, suggestions, meta });
+  res.status(200).render("admin/suggestions", { ext, suggestions, meta });
 });
 
 /*
@@ -130,71 +145,71 @@ exports.suggestions = catchAsync(async (req, res, next) => {
 // ==================== forms
 exports.movieform = catchAsync(async (req, res, next) => {
   const ext = {
-    title: 'Create Movie',
-    page: 'movieform',
-    searchPlaceholder: '',
+    title: "Create Movie",
+    page: "movieform",
+    searchPlaceholder: "",
     js: `${jsPath}/movieform`,
   };
 
-  res.status(200).render('admin/movieform', { ext });
+  res.status(200).render("admin/movieform", { ext });
 });
 
 exports.gameform = catchAsync(async (req, res, next) => {
   const ext = {
-    title: 'Create Game',
-    page: 'gameform',
-    searchPlaceholder: '',
+    title: "Create Game",
+    page: "gameform",
+    searchPlaceholder: "",
     js: `${jsPath}/gameform`,
   };
 
-  res.status(200).render('admin/gameform', { ext });
+  res.status(200).render("admin/gameform", { ext });
 });
 
 exports.serieform = catchAsync(async (req, res, next) => {
   const ext = {
-    title: 'Create Movie',
-    page: 'serieform',
-    searchPlaceholder: '',
+    title: "Create Movie",
+    page: "serieform",
+    searchPlaceholder: "",
     js: `${jsPath}/serieform`,
   };
 
-  res.status(200).render('admin/serieform', { ext });
+  res.status(200).render("admin/serieform", { ext });
 });
 
 exports.seasonform = catchAsync(async (req, res, next) => {
   const { serieId } = req.params;
   const ext = {
-    title: 'Create Movie',
-    page: 'seasonform',
-    searchPlaceholder: '',
+    title: "Create Movie",
+    page: "seasonform",
+    searchPlaceholder: "",
     js: `${jsPath}/seasonform`,
   };
 
-  res.status(200).render('admin/seasonform', { ext, serieId });
+  res.status(200).render("admin/seasonform", { ext, serieId });
 });
 
 //
 exports.scheduleform = catchAsync(async (req, res, next) => {
   const ext = {
-    title: 'Create Schedule',
-    page: 'scheduleform',
-    searchPlaceholder: '',
+    title: "Create Schedule",
+    page: "scheduleform",
+    searchPlaceholder: "",
     js: `${jsPath}/scheduleform`,
   };
 
-  res.status(200).render('admin/scheduleform', { ext });
+  res.status(200).render("admin/scheduleform", { ext });
 });
 
 exports.episodeform = catchAsync(async (req, res, next) => {
   const { seasonId } = req.params;
   const ext = {
-    title: 'Create Movie',
-    page: 'episodeform',
-    searchPlaceholder: '',
+    title: "Create Movie",
+    page: "episodeform",
+    searchPlaceholder: "",
     js: `${jsPath}/seasonform`,
   };
 
-  res.status(200).render('admin/episodeform', { ext, seasonId });
+  res.status(200).render("admin/episodeform", { ext, seasonId });
 });
 
 /*
@@ -209,12 +224,12 @@ exports.updatemovie = catchAsync(async (req, res, next) => {
 
   const ext = {
     title: `Update ${movie.title}`,
-    page: 'movieform',
-    searchPlaceholder: 'in movies',
+    page: "movieform",
+    searchPlaceholder: "in movies",
     js: `${jsPath}/movieform`,
   };
 
-  res.status(200).render('admin/movieform', { ext, movie });
+  res.status(200).render("admin/movieform", { ext, movie });
 });
 
 //
@@ -223,12 +238,12 @@ exports.updategame = catchAsync(async (req, res, next) => {
 
   const ext = {
     title: `Update ${game.title}`,
-    page: 'gameform',
-    searchPlaceholder: 'in games',
+    page: "gameform",
+    searchPlaceholder: "in games",
     js: `${jsPath}/gameform`,
   };
 
-  res.status(200).render('admin/gameform', { ext, game });
+  res.status(200).render("admin/gameform", { ext, game });
 });
 
 //
@@ -237,13 +252,13 @@ exports.aboutgame = catchAsync(async (req, res, next) => {
 
   const ext = {
     title: `Update ${game.title}`,
-    page: 'gameform',
-    searchPlaceholder: 'in games',
+    page: "gameform",
+    searchPlaceholder: "in games",
     js: `${jsPath}/gameform`,
     about: true,
   };
 
-  res.status(200).render('admin/gameabout', { ext, game });
+  res.status(200).render("admin/gameabout", { ext, game });
 });
 
 //
@@ -252,12 +267,12 @@ exports.updateserie = catchAsync(async (req, res, next) => {
 
   const ext = {
     title: `Update ${serie.title}`,
-    page: 'serieform',
-    searchPlaceholder: 'in series',
+    page: "serieform",
+    searchPlaceholder: "in series",
     js: `${jsPath}/serieform`,
   };
 
-  res.status(200).render('admin/serieform', { ext, serie });
+  res.status(200).render("admin/serieform", { ext, serie });
 });
 
 //
@@ -266,12 +281,12 @@ exports.updateseason = catchAsync(async (req, res, next) => {
 
   const ext = {
     title: `Update ${season.title}`,
-    page: 'seasonform',
-    searchPlaceholder: 'in seasons',
+    page: "seasonform",
+    searchPlaceholder: "in seasons",
     js: `${jsPath}/seasonform`,
   };
 
-  res.status(200).render('admin/seasonform', { ext, season });
+  res.status(200).render("admin/seasonform", { ext, season });
 });
 
 //
@@ -280,24 +295,27 @@ exports.updateepisode = catchAsync(async (req, res, next) => {
 
   const ext = {
     title: `Update ${episode.title}`,
-    page: 'episodeform',
-    searchPlaceholder: 'in episodes',
+    page: "episodeform",
+    searchPlaceholder: "in episodes",
     js: `${jsPath}/seasonform`,
   };
 
-  res.status(200).render('admin/episodeform', { ext, episode });
+  res.status(200).render("admin/episodeform", { ext, episode });
 });
 
 //
 exports.updateschedule = catchAsync(async (req, res, next) => {
-  const { data: schedule } = await getRequest(req, `/schedules/${req.params.id}`);
+  const { data: schedule } = await getRequest(
+    req,
+    `/schedules/${req.params.id}`
+  );
 
   const ext = {
     title: `Update Schedule`,
-    page: 'scheduleform',
-    searchPlaceholder: 'in schedules',
+    page: "scheduleform",
+    searchPlaceholder: "in schedules",
     js: `${jsPath}/scheduleform`,
   };
 
-  res.status(200).render('admin/scheduleform', { ext, schedule });
+  res.status(200).render("admin/scheduleform", { ext, schedule });
 });
